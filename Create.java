75560d8fb98a4a0aa2508c499e5d5794d484b6f3
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-08-12 19:36:20 +0530

    Stacks

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
+import java.util.Stack;
+import java.util.Iterator;
+
+
 public class Create{
 public static void main(String[]args){
 // Create an Object for Stack
   // Load Values in Stack
   // Iterate it by usig Iterator and While Loops
+   Stack<Integer> stack = new Stack<>();
+   stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        
+        Iterator<Integer> iterator = stack.iterator();
+        while (iterator.hasNext()) {
+          Integer value = iterator.next();
+          System.out.println(value); 
+      }
 }
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,38 @@
-public class Dynamic{
-public static void main(String[]args){
-// Create an Object for Stack
-  // Load person class Objects in it using push function
-  // Iterate it by using List Iterator
-  // print the values by using while Loop
+import java.util.Stack;
+import java.util.ListIterator;
+
+
+class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
 }
+
+public class Dynamic {
+    public static void main(String[] args) {
+        Stack<Person> stack = new Stack<>();
+    
+        stack.push(new Person("Ritesh", 22));
+        ListIterator<Person> iterator = stack.listIterator();
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            System.out.println(person); 
+        }
+    }
 }
